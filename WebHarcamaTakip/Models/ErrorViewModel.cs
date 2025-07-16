0ea7c50664f4d101b69b9ba513de39b77896b368
Faruk Serdar DEGIRMI <farukserdar.degirmi@gunhukuk.com.tr>
2025-07-16 12:21:42 +0300

    İlk kurulum: ASP.NET Core MVC uygulaması başlatıldı

---
+++ WebHarcamaTakip/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebHarcamaTakip.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
