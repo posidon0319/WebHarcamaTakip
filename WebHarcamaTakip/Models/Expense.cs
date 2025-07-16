c05a839a686f5afde92803296c6dbdc44f66019b
Faruk Serdar DEGIRMI <farukserdar.degirmi@gunhukuk.com.tr>
2025-07-16 12:28:38 +0300

    Expense modeli eklendi

---
+++ WebHarcamaTakip/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace WebHarcamaTakip.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
